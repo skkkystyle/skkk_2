d9eb49f0d6b2dbb3bf1de1dd69a8700140d6e39f
Pysin Artem Aleksandrovich <144371806+skkkystyle@users.noreply.github.com>
2024-03-14 13:44:48 +0300

    divided realization in header

--- inc/Stackskkk.hpp
+++ inc/Stackskkk.hpp
@@ -5,6 +5,7 @@
 constexpr int START_CAPACITY = 20;
 constexpr int MEMORY_COEFFICIENT = 2;
 
+
 template<typename T>
 struct Stack
 {
@@ -15,144 +16,178 @@ private:
     T *data;
 
 public:
-    Stack()
-    {
-        capacity = START_CAPACITY;
-        cap = -1;
-        size = 0;
-        data = new T[capacity];
-    }
+    Stack();
+    Stack(const Stack& other);
+    Stack& operator= (const Stack& other);
+    Stack(Stack&& other) noexcept;
+    Stack& operator= (Stack&& other) noexcept;
+    ~Stack();
 
-    Stack(const Stack& other)
+    void push(const T &item);
+//    void push(const T &&item);
+    void pop();
+    T top() const;
+    bool isequal(const Stack& other) const;
+    [[nodiscard]] bool isempty() const;
+    [[nodiscard]] int wsize() const;
+};
+
+
+template<typename T>
+Stack<T>::Stack()
+{
+    capacity = START_CAPACITY;
+    cap = -1;
+    size = 0;
+    data = new T[capacity];
+}
+
+template<typename T>
+Stack<T>::Stack(const Stack &other)
+{
+    capacity = other.capacity;
+    cap = other.cap;
+    size = other.size;
+    data = new T[capacity];
+    for (int i = 0; i <= cap; i++)
+    {
+        data[i] = other.data[i];
+    }
+}
+
+template<typename T>
+Stack<T> &Stack<T>::operator=(const Stack &other)
+{
+    if (this != &other)
     {
         capacity = other.capacity;
         cap = other.cap;
         size = other.size;
+        delete[] data;
         data = new T[capacity];
         for (int i = 0; i <= cap; i++)
         {
             data[i] = other.data[i];
         }
     }
+    return *this;
+}
 
-    Stack& operator= (const Stack& other)
-    {
-        if (this != &other)
-        {
-            capacity = other.capacity;
-            cap = other.cap;
-            size = other.size;
-            delete[] data;
-            data = new T[capacity];
-            for (int i = 0; i <= cap; i++)
-            {
-                data[i] = other.data[i];
-            }
-        }
-        return *this;
-    }
+template<typename T>
+Stack<T>::Stack(Stack &&other) noexcept
+{
+    capacity = other.capacity;
+    cap = other.cap;
+    size = other.size;
+    data = other.data;
+    other.capacity = 0;
+    other.cap = -1;
+    other.size = 0;
+    other.data = nullptr;
+}
 
-    Stack(Stack&& other) noexcept
+template<typename T>
+Stack<T> &Stack<T>::operator=(Stack &&other) noexcept
+{
+    if (this != &other)
     {
         capacity = other.capacity;
         cap = other.cap;
         size = other.size;
+        delete[] data;
         data = other.data;
         other.capacity = 0;
         other.cap = -1;
         other.size = 0;
         other.data = nullptr;
     }
+    return *this;
+}
 
-    Stack& operator= (Stack&& other) noexcept
+template<typename T>
+Stack<T>::~Stack()
+{
+    delete[] data;
+}
+
+template<typename T>
+void Stack<T>::push(const T &item)
+{
+    if (cap == capacity - 1)
     {
-        if (this != &other)
+        T *newdata = new T[capacity * MEMORY_COEFFICIENT];
+        for (int i = 0; i < capacity; i++)
         {
-            capacity = other.capacity;
-            cap = other.cap;
-            size = other.size;
-            delete[] data;
-            data = other.data;
-            other.capacity = 0;
-            other.cap = -1;
-            other.size = 0;
-            other.data = nullptr;
+            newdata[i] = data[i];
         }
-        return *this;
-    }
-
-    ~Stack()
-    {
         delete[] data;
+        data = newdata;
+        capacity *= MEMORY_COEFFICIENT;
     }
+    data[++cap] = item;
+    size++;
+}
 
-    void push(const T &item)
+//template<typename T>
+//void Stack<T>::push(const T &&item)
+//{
+//    return;
+//}
+
+template<typename T>
+void Stack<T>::pop()
+{
+    if (cap == -1)
     {
-        if (cap == capacity - 1)
-        {
-            T *newdata = new T[capacity * MEMORY_COEFFICIENT];
-            for (int i = 0; i < capacity; i++)
-            {
-                newdata[i] = data[i];
-            }
-            delete[] data;
-            data = newdata;
-            capacity *= MEMORY_COEFFICIENT;
-        }
-        data[++cap] = item;
-        size++;
+        throw "TRUE Stack is empty! Nothing to pop. ";
     }
+    size--;
+    cap--;
+    data[cap + 1];
+}
 
-    void pop()
+template<typename T>
+T Stack<T>::top() const
+{
+    if (cap == -1)
     {
-        if (cap == -1)
-        {
-            throw "TRUE Stack is empty! Nothing to pop. ";
-        }
-        size--;
-        cap--;
-        data[cap + 1];
+        throw "TRUE Stack is empty! Nothing on top. ";
     }
+    return data[cap];
+}
 
-    T top() const
+template<typename T>
+bool Stack<T>::isequal(const Stack &other) const
+{
+    if (size == other.size && capacity == other.capacity)
     {
-        if (cap == -1)
+        if (data == other.data)
         {
-            throw "TRUE Stack is empty! Nothing on top. ";
-        }
-        return data[cap];
-    }
-
-    bool isequal(const Stack& other) const
-    {
-        if (size == other.size && capacity == other.capacity)
-        {
-            if (data == other.data)
-            {
-                return true;
-            }
-            for (int i = 0; i < size; i++)
-            {
-                if (data[i] != other.data[i])
-                {
-                    return false;
-                }
-            }
             return true;
         }
-        return false;
+        for (int i = 0; i < size; i++)
+        {
+            if (data[i] != other.data[i])
+            {
+                return false;
+            }
+        }
+        return true;
     }
+    return false;
+}
 
-    [[nodiscard]] bool isempty() const
-    {
-        return cap == -1;
-    }
+template<typename T>
+bool Stack<T>::isempty() const
+{
+    return cap == -1;
+}
 
-    [[nodiscard]] int wsize() const
-    {
-        return size;
-    }
-};
+template<typename T>
+int Stack<T>::wsize() const
+{
+    return size;
+}
 
 
 #endif
